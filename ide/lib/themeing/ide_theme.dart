37af6097b7d898a1a846b26ac6e4bb4832599feb
Norbert <norbertkozsir@gmail.com>
2019-01-21 20:16:02 +0100

    Working on themeing

--- ide/lib/themeing/ide_theme.dart
+++ ide/lib/themeing/ide_theme.dart
@@ -5,9 +5,16 @@ import 'package:flutter/material.dart';
 
 class IDETheme {
 
+  /*IDETheme({
+
+});*/
+
+ // final TextStyle head;
 
   final Color darkBackground = Color(0xff1d1d1d);
   final Color darkerBackground = Color(0xff1f1f1f);
   final Color lightBackground = Color(0xff313131);
 
+  final Color fontColor = Colors.white;
+//  final TextStyle header = TextStyle(color: fontColor);
 }
