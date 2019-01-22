24556112f1837b14ab40579837acffdb9f29fc3e
Norbert <norbertkozsir@gmail.com>
2019-01-22 14:52:35 +0100

    working on the editors

--- ide/lib/ui/widget_editors/property_changers/numeric_values.dart
+++ ide/lib/ui/widget_editors/property_changers/numeric_values.dart
@@ -122,3 +122,19 @@ class ChangeableSize extends StatelessWidget {
     );
   }
 }
+
+class ChangeableConstraints extends StatelessWidget {
+
+  const ChangeableConstraints({Key key, this.onMinWidthChange, this.onMaxWidgetChange, this.onMinHeightChange, this.onMaxHeightChange}) : super(key: key);
+
+  final ValueChanged<double> onMinWidthChange;
+  final ValueChanged<double> onMaxWidgetChange;
+  final ValueChanged<double> onMinHeightChange;
+  final ValueChanged<double> onMaxHeightChange;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
