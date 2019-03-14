02522aa5c90fc0c0759441acfeb93433b6ac5b56
Norbert <norbertkozsir@gmail.com>
2019-03-14 20:10:37 +0100

    Implement basic file system explorer

--- ide/lib/widgets/file_system_explorer.dart
+++ ide/lib/widgets/file_system_explorer.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'dart:io';
 import 'package:path/path.dart' as path;
+import 'package:provider/provider.dart';
 
 
 enum SearchFor {
@@ -23,9 +24,24 @@ class FileSystemExplorer extends StatefulWidget {
 class _FileSystemExplorerState extends State<FileSystemExplorer> {
 
 
+  String _selectedEntity;
+  set selectedEntity(String it) {
+    setState(() {
+      _selectedEntity = it;
+    });
+  }
+
+  static void setSelectedEntity(BuildContext context, String selectedEntity) {
+    _FileSystemExplorerState state = context.ancestorStateOfType(TypeMatcher<_FileSystemExplorerState>());
+    state.selectedEntity = selectedEntity;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Provider<String>(
+      value: _selectedEntity,
+      child: Container()
+    );
   }
 }
 
@@ -48,15 +64,54 @@ class _FolderState extends State<_Folder> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text(path.basename(widget.directory.path)),
+    return Column(
+      children: <Widget>[
+        GestureDetector(
+          onTap: () {
+            _FileSystemExplorerState.setSelectedEntity(context, widget.directory.path);
+          },
+          onDoubleTap: () {
+            setState(() {
+              open = !open;
+            });
+          },
+          child: Container(
+            child: Text(path.basename(widget.directory.path)),
+          ),
+        ),
+        FutureBuilder<List<FileSystemEntity>>(
+          future: widget.directory.list().toList(),
+          builder: (context, snapshot) {
+            if(!snapshot.hasData) return CircularProgressIndicator();
+            return Column(
+              children: snapshot.data.map((it) {
+                if(it is Directory) return _Folder(directory: it,);
+                if(it is File) return _File(file: it);
+              }).toList(),
+            );
+          },
+        ),
+      ],
     );
   }
+
 }
 
 class _File extends StatelessWidget {
+
+  const _File({Key key, this.file}) : super(key: key);
+
+  final File file;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return GestureDetector(
+      onTap: () {
+        _FileSystemExplorerState.setSelectedEntity(context, file.path);
+      },
+      child: Container(
+        child: Text(path.basename(file.path)),
+      ),
+    );
   }
 }
