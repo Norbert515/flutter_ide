53a7dc1a04f66a30817d1f69610f742493198f46
Norbert <norbertkozsir@gmail.com>
2020-05-07 21:36:28 +0200

    Delete old flutter-ide

--- visual_components/flutter_visual_builder/bin/server.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter_visual_builder/business_logic/server/server.dart';
-import 'package:grpc/grpc.dart';
-
-Future<void> main(List<String> args) async {
-  final server = new Server([EditorServer()]);
-  await server.serve(port: 50051);
-  print('Server listening on port ${server.port}...');
-}
--- visual_components/flutter_visual_builder/lib/business_logic/config.dart
+++
@@ -1,2 +0,0 @@
-
-const bool debug = false;
